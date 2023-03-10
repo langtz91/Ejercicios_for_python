61d68d2645bf2f7ddb50685dd40e3592f0980618
Fabian Gutierrez <fabiandavidgutierrez91@gmail.com>
2023-03-10 02:49:36 -0500

    Detallitos

--- Ejer_Fibonacci.py
+++ Ejer_Fibonacci.py
@@ -1,11 +1,10 @@
 limite = int(input("Ingrese el número límite: "))
 x2 = 0
 x3 = 1
-print(x2)
-print (x3)
 
-for i in range(1, limite):
+for i in range(0, limite):
  x1 = x2
  x2 = x3 
  x3 = x1 + x2
- print(x3)
+
+ print(x1)
